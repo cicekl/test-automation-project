e8c8dfbad52039f46d85719e0e84f8b3b014c9ba
Lorena <cicek.lorena@gmail.com>
2026-01-28 18:32:46 +0100

    add cross browser testing.

--- automation-framework/src/test/java/BaseTest.java
+++ automation-framework/src/test/java/BaseTest.java
@@ -1,23 +1,37 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
-public class BaseTest {
+public abstract class BaseTest {
 
     protected WebDriver driver;
 
-
-
     @BeforeMethod(alwaysRun = true)
-    public void setupDriver() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    @Parameters("browser")
+    public void setupDriver(@Optional("chrome")String browser) {
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
     }
 
     @AfterMethod(alwaysRun = true)
